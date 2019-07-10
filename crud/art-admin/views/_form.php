7807d72d6c7c86b792a13f59a28ce189dbf30dfe
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2019-07-10 12:21:55 +0300

    Update _form.php

--- crud/art-admin/views/_form.php
+++ crud/art-admin/views/_form.php
@@ -35,7 +35,7 @@ use artsoft\helpers\Html;
     ?>
 
     <div class="row">
-        <div class="col-md-9">
+        <div class="col-md-8">
 
             <div class="panel panel-default">
                 <div class="panel-body">
@@ -50,7 +50,7 @@ use artsoft\helpers\Html;
             </div>
         </div>
 
-        <div class="col-md-3">
+        <div class="col-md-4">
 
             <div class="panel panel-default">
                 <div class="panel-body">
