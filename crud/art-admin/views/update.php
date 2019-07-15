394bd89fa39e6e45096a3fea34d1200b6af25132
Markov Artur <36476213+artmarkov@users.noreply.github.com>
2019-07-15 16:32:20 +0300

    Update update.php

--- crud/art-admin/views/update.php
+++ crud/art-admin/views/update.php
@@ -16,7 +16,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model <?= ltrim($generator->modelClass, '\\') ?> */
 
-$this->title = Yii::t('art', 'Update "{item}"', ['item' => $model->id];
+$this->title = Yii::t('art', 'Update "{item}"', ['item' => $model->id]);
 $this->params['breadcrumbs'][] = ['label' => Yii::t('<?= $generator->messageCategory ?>', <?= $generator->generateString(Inflector::pluralize(Inflector::camel2words(StringHelper::basename($generator->modelClass)))) ?>), 'url' => ['default/index']];
 $this->params['breadcrumbs'][] = ['label' => $model-><?= $generator->getNameAttribute() ?>, 'url' => ['default/view', <?= $urlParams ?>]];
 $this->params['breadcrumbs'][] = <?= $generator->generateString('Update') ?>;
